effc76ab54676955950e81038a8f078fd8598d8f
Daniel Olsson <fipaso@gmail.com>
2015-04-09 00:05:57 +0200

    reverted

---
+++ test/memoryserver.d
@@ -0,0 +1,3 @@
+memoryserver.o memoryserver.d : memoryserver.cc ../server.h ../connection.h ../protocol.h \
+ ../connection.h ../connectionclosedexception.h newsgroup.h article.h \
+ messagehandler.cc
