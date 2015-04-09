a6cbe50e3ad8c6747358c3a023814f4d682a8725
Oscar <ockka@email.com>
2015-04-09 12:45:08 +0200

    merge

---
+++ test/myclient.d
@@ -0,0 +1,2 @@
+myclient.o myclient.d : myclient.cc ../connection.h ../connectionclosedexception.h \
+ messagehandler.cc ../protocol.h
---
+++ test/myserver.d
@@ -0,0 +1,3 @@
+myserver.o myserver.d : myserver.cc ../server.h ../connection.h ../protocol.h \
+ ../connection.h ../connectionclosedexception.h newsgroup.h article.h \
+ messagehandler.cc
