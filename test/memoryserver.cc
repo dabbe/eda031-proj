eb7cd5aac5eb67c04f9e865c8250845a1b888578
Hchlasmuck <axel_ahlbeck@hotmail.se>
2015-04-09 15:59:14 +0200

    memoryserver half-compiling

---
+++ test/memoryserver.cc
@@ -0,0 +1,41 @@
+#include "comserver.h"
+#include "protocol.h"
+#include "connection.h"
+#include "connectionclosedexception.h"
+#include "messagehandler.cc"
+
+#include <algorithm>
+#include <sstream>
+#include <memory>
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <cstdlib>
+
+int main(int argc, char* argv[]){
+	if (argc != 2) {
+		cerr << "Usage: myserver port-number" << endl;
+		exit(1);
+	}
+	
+	int port = -1;
+	try {
+		port = stoi(argv[1]);
+	} catch (exception& e) {
+		cerr << "Wrong port number. " << e.what() << endl;
+		exit(1);
+	}
+
+	ComServer cs(port);
+
+	if (!cs.isInitialized()) {
+		cerr << "Server initialization error." << endl;
+		exit(1);
+	}
+	vector<Newsgroup> ngs;
+	unsigned int ngcounter = 0;
+
+
+
+	return 0;
+}
